4971ac2c98cff72c3ce350f1417c24aa0566a0d3
Purabsingla <purabsingla15@gmail.com>
2025-07-14 18:37:13 +0530

    feat: update dependencies and implement UI components with Tailwind CSS and Radix UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-gray-200">
+      <Button variant="destructive">outline</Button>
+      <Progress value={73} className="top-40 w-1/2" />
+    </div>
   );
 }
 
